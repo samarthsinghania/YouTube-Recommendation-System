2d4e49d0a7b6e3cbb8c2b46565290176b9a50937
sam <samarthsinghania0@gmail.com>
2026-06-05 23:39:59 +0530

    add main_control class in main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+class main_control:
+    def __init__(self):
+        self.vidid = None
+
+    
--- testing_purposes.py
+++ testing_purposes.py
@@ -1,23 +1,38 @@
-# import json as js
-# l = list({1,2,3,4})
-# with open('vectorformat.json','w') as f:
-#     js.dump(l,f)
+# # import json as js
+# # l = list({1,2,3,4})
+# # with open('vectorformat.json','w') as f:
+# #     js.dump(l,f)
+# import numpy as np
+
+# from file_functions import Fily
+
+# # v = np.zeros(5,dtype=np.int8)
+# # v[3]=1
+
+# # print(v.tolist())
+
+# #test:
+# obj = Fily()
+
+# # s = {'pizza', 'pineapple','ketchup',"mayo","newyork"}
+# # word = ['pineapple', 'mayo']
+# # print(s)
+# # print(obj.vector_creator(word,s))
+
 import numpy as np
+import json as js
+from main import main_control
 
-from file_functions import Fily
+obj = main_control()
 
-# v = np.zeros(5,dtype=np.int8)
-# v[3]=1
+v = np.array([[0,0,0,1,0,1,0,1,1,0],[0,1,0,1,1,0,0,1,1,0],[1,1,0,1,0,1,1,0,1,0]])
+t = np.array([0,0,0,1,0,1,0,1,1,0])
 
-# print(v.tolist())
+# a_mag = np.linalg.norm(v)
+# b_mag = np.linalg.norm(t)
 
-#test:
-obj = Fily()
+given  = obj.cosine_similar(t,v)
+for i in given:
+    print(i)
 
-# s = {'pizza', 'pineapple','ketchup',"mayo","newyork"}
-# word = ['pineapple', 'mayo']
-# print(s)
-# print(obj.vector_creator(word,s))
-
-
-obj.raw_data_gen("Pizza",4)
+print("hi",given[2])
