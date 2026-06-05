9b67caf0b13dad7c0a40fa05813e46eec180c934
sam <samarthsinghania0@gmail.com>
2026-06-05 23:41:12 +0530

    add cosine_similar method in main_control main.py

--- main.py
+++ main.py
@@ -4,4 +4,21 @@ class main_control:
     def __init__(self):
         self.vidid = None
 
-    
+    def cosine_similar(self, given_vector, main_vector):
+        '''This function takes in 2 Parameters:
+        1. given_vector: the video given vector
+        2. main_vector: the total vector numpy array
+        Returns the vid id of top n'''
+
+        cosine_list = []
+
+        for vector in main_vector:
+            a_mag = np.linalg.norm(given_vector)
+            b_mag = np.linalg.norm(vector)
+
+            dot = np.dot(given_vector,vector)
+
+            cosine_similar = dot/(a_mag*b_mag)
+            cosine_list.append(cosine_similar)
+        
+        return np.array(cosine_list)
