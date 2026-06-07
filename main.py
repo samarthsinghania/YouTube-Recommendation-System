f606509b01ef80a856fd69395ea86603ab7b6974
sam <samarthsinghania0@gmail.com>
2026-06-07 16:23:03 +0530

    edit cosine_similar to cosine_similar_top_n in main_control of main.py

--- main.py
+++ main.py
@@ -1,24 +1,44 @@
 import numpy as np
+import json as js
 
 class main_control:
     def __init__(self):
         self.vidid = None
 
-    def cosine_similar(self, given_vector, main_vector):
+    def cosine_similar_top_n(self, given_vector_id, quantity):
         '''This function takes in 2 Parameters:
-        1. given_vector: the video given vector
-        2. main_vector: the total vector numpy array
-        Returns the vid id of top n'''
+        1. given_vector: the video id of the given vector
+        2. quantiy (n): the top n  cosine similar's videoid(s)
+        Returns the vid id of top n in list format'''
+
+        
+        with open("vector.json", "r") as f:
+            vidinfo = js.load(f)
+            
+        #its(vidinfo) in form: d ={'vidid': vector,..}
+        
+        given_vector = vidinfo[given_vector_id]
 
         cosine_list = []
+        for vidid in vidinfo:
+            if given_vector_id==vidid: #ignore if given is there
+                pass
+            else:
+                vector = vidinfo[vidid]
+                a_mag = np.linalg.norm(given_vector) #magnitudes 
+                b_mag = np.linalg.norm(vector)
 
-        for vector in main_vector:
-            a_mag = np.linalg.norm(given_vector)
-            b_mag = np.linalg.norm(vector)
+                dot = np.dot(given_vector,vector) #dot product
+    
+                cosine_similar = dot/(a_mag*b_mag) #costheta
+                cosine_list.append((vidid, cosine_similar)) #appending to list
+        #format: [(vidid, cosine_similar),...]
 
-            dot = np.dot(given_vector,vector)
+        #sorting on basis of 2nd element
+        sorted_cosine_list = sorted(cosine_list,key=lambda x:x[1],reverse=True)
 
-            cosine_similar = dot/(a_mag*b_mag)
-            cosine_list.append(cosine_similar)
+        topn=[]
+        for i in range(0,quantity):
+            topn.append(sorted_cosine_list[i][0])
         
-        return np.array(cosine_list)
+        return topn
--- testing_purposes.py
+++ testing_purposes.py
@@ -1,38 +1,14 @@
-# # import json as js
-# # l = list({1,2,3,4})
-# # with open('vectorformat.json','w') as f:
-# #     js.dump(l,f)
-# import numpy as np
-
-# from file_functions import Fily
-
-# # v = np.zeros(5,dtype=np.int8)
-# # v[3]=1
-
-# # print(v.tolist())
-
-# #test:
-# obj = Fily()
-
-# # s = {'pizza', 'pineapple','ketchup',"mayo","newyork"}
-# # word = ['pineapple', 'mayo']
-# # print(s)
-# # print(obj.vector_creator(word,s))
-
-import numpy as np
 import json as js
-from main import main_control
+d ={'a': [1,0,1] ,'b': [0,0,1], 'c': [1,0,1],'d': [1,1,0],'e': [0,0,0],'g': [1,0,1]}
+with open("vector.json", "w") as f:
+    js.dump(d,f)
 
-obj = main_control()
+from main import main_control as m
 
-v = np.array([[0,0,0,1,0,1,0,1,1,0],[0,1,0,1,1,0,0,1,1,0],[1,1,0,1,0,1,1,0,1,0]])
-t = np.array([0,0,0,1,0,1,0,1,1,0])
+test_vector = [1,1,1]
+obj = m()
 
-# a_mag = np.linalg.norm(v)
-# b_mag = np.linalg.norm(t)
+print(obj.cosine_similar_top_n('g',4))
 
-given  = obj.cosine_similar(t,v)
-for i in given:
-    print(i)
-
-print("hi",given[2])
+# l = [1,4,2]
+# print(sorted(l,reverse=True))
