52334ad033b19c96aeeff91dafad76bb5e758bad
sam <samarthsinghania0@gmail.com>
2026-06-13 23:57:51 +0530

    add YouTube home-page videos feature in app.py

--- main_code(s)/app.py
+++ main_code(s)/app.py
@@ -2,11 +2,10 @@ import streamlit as st
 import time 
 import json as js
 from st_clickable_images import clickable_images
+from st_click_detector import click_detector
 
 #Page title 
-st.set_page_config(page_title="YT RS")
-#icon
-st.set_page_config(page_title="Surge Price Prediction App", page_icon = 'resources\icon.png')
+st.set_page_config(page_title="YT RS", page_icon = 'resources/icon.png',layout="wide")
 
 
 st.header('YouTube Recommendation System')
@@ -14,17 +13,35 @@ st.header('YouTube Recommendation System')
 
 
 #for wide streamlit app
-st.set_page_config(layout="wide")
 
-#*********
 
-# image_lis = [
-#         "https://images.unsplash.com/photo-1565372195458-9de0b320ef04?w=700",
-#         "https://images.unsplash.com/photo-1565372195458-9de0b320ef04?w=700",
-#         "https://images.unsplash.com/photo-1582550945154-66ea8fff25e1?w=700",
-#         "https://images.unsplash.com/photo-1591797442444-039f23ddcc14?w=700",
-#         "https://images.unsplash.com/photo-1518727818782-ed5341dbd476?w=700",
-#     ]
+#for removing streamlit's watermark and the default menu option
+hide_default_format = """
+       <style>
+       #MainMenu {visibility: hidden; }
+       footer {visibility: hidden;}
+       </style>
+       """
+st.markdown(hide_default_format, unsafe_allow_html=True)
+
+#tex
+st.text('HELLO THIS IS AN APPs')
+
+#sidebar
+with st.sidebar:
+    st.text("HEy Welcome to Youtube")
+    st.image("https://images.unsplash.com/photo-1565372195458-9de0b320ef04?w=700")
+
+
+image_lis = [
+        "https://images.unsplash.com/photo-1565372195458-9de0b320ef04?w=700",
+        "https://images.unsplash.com/photo-1565372195458-9de0b320ef04?w=700",
+        "https://images.unsplash.com/photo-1582550945154-66ea8fff25e1?w=700",
+        "https://images.unsplash.com/photo-1591797442444-039f23ddcc14?w=700",
+        "https://images.unsplash.com/photo-1518727818782-ed5341dbd476?w=700",
+    ]
+
+image_lis2 = ["https://images.unsplash.com/photo-1565372195458-9de0b320ef04?w=700"]
 # clicked = clickable_images(
 #     image_lis,
 #     titles=[f"Image #{str(i)}" for i in range(5)], #[Image 0, Image 1..basically]
@@ -33,3 +50,119 @@ st.set_page_config(layout="wide")
 # )
 # st.markdown(f"Image #{clicked} clicked" if clicked > -1 else "No image clicked")
 
+content = f"""
+<div style="display:flex; justify-content:center; gap:50px;">
+
+    <div style="width:350px;">
+        <a href='#' id='vid1'>
+            <img
+                src='{image_lis[0]}'
+                style='width:100%; border-radius:12px;'
+            >
+        </a>
+
+        <h4 style='margin-bottom:0px;'>
+            Linear Regression Explained
+        </h4>
+
+        <p style='color:gray; margin-top:5px;'>
+            StatQuest
+        </p>
+    </div>
+
+    <div style="width:350px;">
+        <a href='#' id='vid2'>
+            <img
+                src='{image_lis[1]}'
+                style='width:100%; border-radius:12px;'
+            >
+        </a>
+
+        <h4 style='margin-bottom:0px;'>
+            Neural Networks Amazing video man just watch it!
+        </h4>
+
+        <p style='color:gray; margin-top:5px;'>
+            the young technical guy
+        </p>
+    </div>
+
+    <div style="width:350px;">
+        <a href='#' id='vid3'>
+            <img
+                src='{image_lis[1]}'
+                style='width:100%; border-radius:12px;'
+            >
+        </a>
+
+        <h4 style='margin-bottom:0px;'>
+            Neural Networks
+        </h4>
+
+        <p style='color:gray; margin-top:5px;'>
+            3Blue1Brown
+        </p>
+    </div>
+
+</div>
+
+<div style="display:flex; justify-content:center; gap:50px;">
+
+    <div style="width:350px;">
+        <a href='#' id='vid4'>
+            <img
+                src='{image_lis[0]}'
+                style='width:100%; border-radius:12px;'
+            >
+        </a>
+
+        <h4 style='margin-bottom:0px;'>
+            Linear Regression Explained
+        </h4>
+
+        <p style='color:gray; margin-top:5px;'>
+            StatQuest
+        </p>
+    </div>
+
+    <div style="width:350px;">
+        <a href='#' id='vid5'>
+            <img
+                src='{image_lis[1]}'
+                style='width:100%; border-radius:12px;'
+            >
+        </a>
+
+        <h4 style='margin-bottom:0px;'>
+            Neural Networks Amazing video man just watch it!
+        </h4>
+
+        <p style='color:gray; margin-top:5px;'>
+            the young technical guy
+        </p>
+    </div>
+
+    <div style="width:350px;">
+        <a href='#' id='vid6'>
+            <img
+                src='{image_lis[1]}'
+                style='width:100%; border-radius:12px;'
+            >
+        </a>
+
+        <h4 style='margin-bottom:0px;'>
+            Neural Networks
+        </h4>
+
+        <p style='color:gray; margin-top:5px;'>
+            3Blue1Brown
+        </p>
+    </div>
+
+</div>
+"""
+
+
+clicked = click_detector(content)
+
+st.markdown(f"**{clicked} clicked**" if clicked != "" else "**No click**")
--- main_code(s)/testing_purposes.py
+++ main_code(s)/testing_purposes.py
@@ -1,55 +1,5 @@
-import json as js
-from file_functions import Fily
-from core_functions import main_control
+l = ['yo','hi',3]
+print(enumerate(l))
 
-# with open('main_code(s)/Json_files/video_words.json', 'w') as f:
-#     w = js.dump({},f)
-
-# # print(len(w)
-
-obj = Fily()
-
-# obj.video_all_words_gen()
-
-# # obj.bad_words_deleter()
-
-
-# with open('main_code(s)/Json_files/all_video_words.json', 'r') as f:
-#     l = js.load(f)
-# print(len(l))
-# print(obj.all_vector_words_creator())
-
-# L = ['Hi','meow','cat']
-# # print(set(L))
-
-# print((",".join(L)).lower().split(','))
-
-
-
-#to convert everything to lower
-# with open('main_code(s)/Json_files/video_words.json', 'r') as f:
-#     dic = js.load(f)
-
-# new_dic ={}
-# for vid_id in dic:
-#     lis = dic[vid_id]
-#     new_lis = (",".join(lis)).lower().split(',')
-#     new_dic[vid_id] = new_lis
-# with open('main_code(s)/Json_files/video_words.json', 'w') as f:
-#     js.dump(new_dic,f)
-
-"Making vector.json"
-# obj.all_videos_vector_json_maker()
-
-
-"MAIN"
-
-o = main_control()
-
-import time
-
-current_time=time.time()
-
-print(o.cosine_similar_top_n('1etaZ5ITXlg',2))
-
-print("time taken: ",time.time()-current_time)
+for i in enumerate(l):
+    print(i)
