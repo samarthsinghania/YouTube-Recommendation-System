aa12ed8f706abc6f237446c1b8ee45e23f48602a
sam <samarthsinghania0@gmail.com>
2026-06-15 18:29:28 +0530

    add random_vid_id_sender method of main_control in core_functions.py

--- main_code(s)/core_functions.py
+++ main_code(s)/core_functions.py
@@ -1,5 +1,6 @@
 import numpy as np
 import json as js
+import random
 
 class main_control:
     def __init__(self):
@@ -44,3 +45,21 @@ class main_control:
         
         return topn
     
+    # def update_vid_recommender(self, vid_id):
+    #     top_9 = self.cosine_similar_top_n(vid_id,9)
+
+    def random_vid_id_sender(self, how_many):
+        '''This Method returns random videos's id in a list format,
+        1. how_many : ho  w many videos you want'''
+
+        with open("main_code(s)/json_files/normal_data.json") as f:
+            normal_dic = js.load(f)
+
+        all_keys = list(normal_dic.keys()) #random throws TypeError in .keys() dtype('dict_keys' object)
+
+        random_videos = []
+        for i in range(how_many):
+            random_videos.append(random.choice(all_keys))
+        
+        return random_videos
+
--- main_code(s)/testing_purposes.py
+++ main_code(s)/testing_purposes.py
@@ -1,5 +1,11 @@
 from file_functions import Fily
+from core_functions import main_control as m
+import random
 
 obj = Fily()
 
-obj.cache_updater(['vid', ['v1', 'v2']])
+# obj.cache_updater(['vid', ['v1', 'v2']])
+
+ob = m()
+
+print(ob.random_vid_id_sender(4))
