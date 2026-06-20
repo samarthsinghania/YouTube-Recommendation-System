99350e3992028a3803ec576adb61c0947ee124f7
sam <samarthsinghania0@gmail.com>
2026-06-20 09:17:44 +0530

    add loading bar logic in reset_button, app.py

--- main_code(s)/app.py
+++ main_code(s)/app.py
@@ -141,3 +141,12 @@ reset_button = st.button("Reset")
 if reset_button:
     fily.update_queue(0,'1')
     core.reset_homepage_to_random()
+
+    #Loading Bar Logic
+    progress_text = "Operation in progress. Please wait."
+    my_bar = st.progress(0, text=progress_text)
+    for percent_complete in range(100):
+        time.sleep(0.01)
+        my_bar.progress(percent_complete + 1, text=progress_text)
+
+    st.rerun()
